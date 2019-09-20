6e50d3292d439f5d44b3bbba52d529ac5800be5a
DarkAngelGR <asavvo01@gmail.com>
2019-09-20 09:44:43 +0000

    EvoX bring up

--- sdm845.mk
+++ sdm845.mk
@@ -14,7 +14,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-superior
+    $(LOCAL_PATH)/overlay-evolution
 
 # Permissions
 PRODUCT_COPY_FILES += \
